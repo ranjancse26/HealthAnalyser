418aaf09c54d2b17dbca6df3c7b130c12d5e42c1
ranjancse <ranjand@live.com>
2013-06-19 22:28:36 -0500

    Mood Data Changes

---
+++ HealthAnalyser/Helpers/MoodDataHelper.cs
@@ -0,0 +1,59 @@
+﻿using HealthAnalyser.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace HealthAnalyser.Helpers
+{
+    public class MoodDataHelper
+    {
+        MoodViewModelEntity viewModel;
+        public MoodDataHelper(MoodViewModelEntity viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        private static string PutIntoQuotes(string value)
+        {
+            return "\"" + value + "\"";
+        }
+
+        public MoodDataViewModel GetViewModel()
+        {
+            DataSet ds = new DataSet();
+            var moodDataViewModel = new MoodDataViewModel();
+            moodDataViewModel.MoodDataEntity = viewModel; 
+
+            ds.ReadXml(string.Format("http://moodpanda.com/api/user/feed/data.ashx?userid={0}&from={1}&to={2}&format=xml&DateOrder=ASC&key=5fe9dd40-1f55-4483-abb0-5540d7bf1b93", viewModel.UserId, viewModel.FromDate.Date.ToString("yyyy-MM-dd"), viewModel.ToDate.Date.ToString("yyyy-MM-dd")));
+            if (ds.Tables.Count == 1)
+            {
+                string lables = "[";
+                string ratingData = "[";
+                foreach (DataRow item in ds.Tables[0].Rows)
+                {
+                    var splittedDate = item["Date"].ToString().Split('T');
+                   
+                    moodDataViewModel.MoodViewModelEntity.Add(new MoodViewModelEntity
+                    {
+                         Rating = int.Parse(item["Rating"].ToString()),
+                         Reason = item["Reason"].ToString(),
+                         Time = splittedDate[1].ToString()
+                    });
+
+                     lables = lables + PutIntoQuotes(splittedDate[0]) + ",";
+
+                    ratingData = ratingData + item["Rating"].ToString() + ",";
+                }
+
+                lables = lables.Substring(0, lables.Length - 1) + "]";
+                ratingData = ratingData.Substring(0, ratingData.Length - 1) + "]";
+
+                moodDataViewModel.MoodDataViewForChart.Labels = lables.Replace(@"\", " ");
+                moodDataViewModel.MoodDataViewForChart.RatingData = ratingData;
+            }
+            return moodDataViewModel;
+        }
+    }
+}
---
+++ HealthAnalyser/Models/MoodViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealthAnalyser.Models
+{
+    public class MoodViewModelEntity
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        public int UserId { get; set; }
+
+        public string Reason { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "From Date")]        
+        public DateTime FromDate { get; set; }
+       
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "To Date")]
+        public DateTime ToDate { get; set; }
+
+        public int Rating { get; set; }
+
+        [UIHint("Hidden")]
+        public string Time { get; set; }
+    }
+
+    public class MoodDataViewModel
+    {
+        public MoodDataViewModel()
+        {
+            this.MoodViewModelEntity = new List<MoodViewModelEntity>();
+            this.MoodDataViewForChart = new MoodDataViewForChart();
+            this.MoodDataEntity = new MoodViewModelEntity();
+        }
+
+        public MoodViewModelEntity MoodDataEntity { get; set; }
+        public List<MoodViewModelEntity> MoodViewModelEntity { get; set; }
+        public MoodDataViewForChart MoodDataViewForChart { get; set; }
+    }
+
+    public class MoodDataViewForChart
+    {
+        public string Labels { get; set; }
+        public string RatingData { get; set; }       
+    }
+}
